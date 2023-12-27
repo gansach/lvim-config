986419725bd9852c4a396d2eb13e4087542f886c
gansach <gasachdeva@microsoft.com>
2023-12-27 14:30:26 +0530

    feat: add harpoon-core and its keybindings

--- config.lua
+++ config.lua
@@ -137,6 +137,35 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    'MeanderingProgrammer/harpoon-core.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('harpoon-core').setup({
+        -- Make existing window active rather than creating a new window
+        use_existing = true,
+        -- Default action when opening a mark, defaults to current window
+        -- Example: 'vs' will open in new vertical split, 'tabnew' will open in new tab
+        default_action = nil,
+        -- Set marks specific to each git branch inside git repository
+        mark_branch = false,
+        -- Use the previous cursor position of marked files when opened
+        use_cursor = true,
+        -- Settings for popup window
+        menu = {
+          width = 60,
+          height = 10,
+        },
+        -- Highlight groups to use for various components
+        highlight_groups = {
+          window = 'HarpoonWindow',
+          border = 'HarpoonBorder',
+        },
+      })
+    end,
+  }
 }
 
 -- Restore the last session
@@ -158,4 +187,12 @@ lvim.builtin.which_key.mappings["t"] = {
 
 lvim.builtin.which_key.mappings["sw"] = {
   "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", "current word"
+}
+
+lvim.builtin.which_key.mappings["m"] = {
+  name = "Marks",
+  a = {"<cmd>lua require('harpoon-core.mark').add_file()<cr>", "Add File"},
+  m = {"<cmd>lua require('harpoon-core.ui').toggle_quick_menu()<cr>", "View marks"},
+  n = {"<cmd>lua require('harpoon-core.ui').nav_next()<cr>", "Goto next mark"},
+  p = {"<cmd>lua require('harpoon-core.ui').nav_prev()<cr>", "Goto prev mark"}
 }
